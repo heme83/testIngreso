6c460e5fcd2893d06ca9527df380a684dd686e21
heme83 <heme85@gmail.com>
2016-07-01 20:04:01 -0300

    este es el primer commit que hago

---
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -0,0 +1,10 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+Mostar el resulto por medio de "ALERT"*/
+function sumar()
+{ var uno= 33
+	var dos=55
+	resultato= parseInt+(uno)+parseInt(dos);
+
+}
+
